b2d8c6e241e47ff7e3f404178740ddd76cd637c0
Dave Marshall <dave@atst.io>
2014-05-31 00:01:57 +0100

    Initial commit

---
+++ app.php
@@ -0,0 +1,67 @@
+<?php
+
+use Services_Twilio_Twiml as Twiml;
+use Silex\Application;
+use Silex\Provider\MonologServiceProvider;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use SendGrid\Email;
+
+require 'vendor/autoload.php';
+
+$app = new Application;
+
+$app->register(new MonologServiceProvider(), [
+    'monolog.logfile' => 'php://stdout',
+]);
+
+$app['sendgrid'] = function ($app) {
+    return new SendGrid(
+        getenv('SENDGRID_USERNAME'),
+        getenv('SENDGRID_PASSWORD')
+    );
+};
+
+$app->post("/voice", function (Request $request, Application $app) {
+
+    $twiml = new Twiml;
+    $twiml->say("Thanks for calling That Podcast! Please leave a message after the beep, pressing any key when you are done. Please be sure to leave an email address so we can contact you.");
+    $twiml->record([
+        'maxLength' => 120,
+        'action' => '/recordings',
+    ]);
+
+    return new Response((string) $twiml);
+});
+
+$app->post("/recordings", function (Request $request, Application $app) {
+
+    $email = new Email();
+
+    $to = getenv('VOICEMAIL_EMAIL_ADDRESS');
+    $app['logger']->info("Sending notification to $to");
+
+    $email->addTo($to)
+        ->setFrom($to)
+        ->setSubject("New Voicemail!")
+        ->setText(sprintf(
+            "SID: %s\nDuration: %s\nURL: %s\n",
+            $request->get("CallSid"),
+            $request->get("RecordingDuration"),
+            $request->get("RecordingUrl")
+        ));
+
+    $rsp = $app['sendgrid']->send($email);
+
+    if (is_object($rsp) && isset($rsp->errors)) {
+        $app['logger']->error(json_encode($rsp));
+    }
+
+    $twiml = new Twiml;
+    $twiml->say("Thanks for leaving a message, you're awesome.");
+    $twiml->hangup();
+
+    return new Response((string) $twiml);
+});
+
+return $app;
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+$app = require __DIR__.'/../app.php';
+
+$app->run();
